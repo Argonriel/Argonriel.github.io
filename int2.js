16bc998181e09067c69554dc5bda66e1cbdc6229
Argonriel <107026754+Argonriel@users.noreply.github.com>
2023-06-05 17:27:26 +0800

    Update int2.js

--- int2.js
+++ int2.js
@@ -33,3 +33,18 @@ app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
+async function sendMessage(message) {
+  try {
+    const response = await axios.post('argonriel.github.io', {
+      message,
+    });
+    const reply = response.data.reply;
+    // Handle the reply from Chat GPT
+    console.log(reply);
+  } catch (error) {
+    console.error('Server error:', error.message);
+  }
+}
+
+// Example usage
+sendMessage('Hello, Chat GPT!');
