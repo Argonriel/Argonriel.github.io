b13fdcd16d665b09fa05e22514bbb2dd1f2e2df4
Arielll <107026754+Argonriel@users.noreply.github.com>
2023-07-06 01:22:48 +0800

    Delete script.js

--- library/script.js
+++
@@ -1,7 +0,0 @@
-// script.js
-fetch('index_clm.html')
-  .then(response => response.text())
-  .then(data => {
-    document.getElementById('externalContent').innerHTML = data;
-  })
-  .catch(error => console.error(error));
